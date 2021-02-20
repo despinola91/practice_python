c552d7094b9887b6dd02f2b59ad35905445e5b13
Dario Espinola <despinola91@gmail.com>
2021-02-20 19:48:26 -0300

    Adding structure details for exercise 34

--- 34_birthday_json.py
+++ 34_birthday_json.py
@@ -11,30 +11,72 @@
 # If you run the program multiple times and keep adding new names, your JSON file should keep getting bigger and bigger.
 
 import json
+fileName = "34_birthdays.json"
+birthdays = dict()
 
-def get_birthdays():
-    with open("34_birthdays.json", "r") as f:
+
+def load_birthdays():
+    global birthdays
+    with open(fileName, "r") as f:
         birthdays = json.load(f)
 
     return birthdays
-    
+
+
 def save_birthdays(birthdays):
-    with open("34_birthdays.json", "w") as f:
+    with open(fileName, "w") as f:
         json.dump(birthdays, f)
 
-def show_birthday(birthdays, name):
-    date = birthdays.get(name, "unknown as we don't have this name in the list!")
+
+def show_birthday(name):
+    date = birthdays.get(name, "unknown as we don't have this name in the list")
     print(">>> {}'s birthday is {}.".format(name, date))
 
-def run():
-    print(">>> Welcome to the birthday dictionary. We know the birthdays of:\n")
 
-    birthdays = get_birthdays()
+def add_birthday(name, birthday):
+    birthdays[name] = birthday
+    save_birthdays(birthdays)
+    print(">>> The new record was added.")
+
+
+def show_complete_list():
+    print(">>> We know the birthdays of:\n")
+    load_birthdays()
     for key in birthdays.keys():
         print(key)
 
+
+def get_birthday():
     name = input("\n>>> Who's birthday do you want to look up?\n")
-    show_birthday(birthdays, name)
+    show_birthday(name)
+
+
+def create_birthday():
+    newName = input(">>> Please enter the name of the person:\n")
+    newBirthday = input(">>> Please enter the person's birthday with format mm/dd/yyyy:\n")
+    add_birthday(newName, newBirthday)
+
+
+def run():
+    print("\n>>> Welcome to the birthday dictionary.")
+
+    while True:
+        option = input(
+        """\n>>> What would you like to do?
+        1. See complete list of available birthdays
+        2. Look up someone's birthday
+        3. Add a new birthday\n\n""")
+        
+        if option == "1":
+            show_complete_list()
+        elif option == "2":
+            get_birthday()
+        elif option == "3":
+            create_birthday()
+        elif option.lower() == "leave" or option.lower() == "exit" or option.lower() == "quit":
+            exit()
+        else:
+            print(">>> The entered value is not valid.")
 
 if __name__ == "__main__":
     run()
